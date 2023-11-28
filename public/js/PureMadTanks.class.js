83e7d203de8cf026d05d1cfd32f1df5306c6b3d9
Andy <amcdonald1307@protonmail.com>
2023-11-28 00:53:09 +0000

    Slight refactor

--- public/js/PureMadTanks.class.js
+++ public/js/PureMadTanks.class.js
@@ -15,11 +15,7 @@ class PureMadTanks extends Game {
     }
 
     init = () => {
-        this.#topBorder = new StaticWorldObject(1.0, 0.5, 0.05, (this._width / 2), 0, this._width, 10, 'topBorder', 'topBorder', 0, this._scale, this._world);
-        this.#bottomBorder = new StaticWorldObject(1.0, 0.5, 0.05, (this._width / 2), this._height, this._width, 10, 'bottomBorder', 'bottomBorder', 0, this._scale, this._world);
-        this.#rightBorder = new StaticWorldObject(1.0, 0.5, 0.05, this._width, (this._height / 2), 10, this._height, 'rightBorder', 'rightBorder', 0, this._scale, this._world);
-        this.#leftBorder = new StaticWorldObject(1.0, 0.5, 0.05, 0, (this._height / 2), 10, this._height, 'leftBorder', 'leftBorder', 0, this._scale, this._world);
-
+        this._spawnAllObjects();
         this._interval = setInterval(() => {
             this.update();
         }, 1000/this._framerate);
@@ -29,6 +25,19 @@ class PureMadTanks extends Game {
 
     };
 
+    _destroyListLogic = () => {
+        for(let i in this._destroyList){
+            this.world.DestroyBody(this._destroyList[i]);
+        }
+        this._destroyList.length = 0;
+    };
+
+    _spawnAllObjects = () => {
+        this.#topBorder = new StaticWorldObject(1.0, 0.5, 0.05, (this._width / 2), 0, this._width, 10, 'topBorder', 'topBorder', 0, this._scale, this._world);
+        this.#bottomBorder = new StaticWorldObject(1.0, 0.5, 0.05, (this._width / 2), this._height, this._width, 10, 'bottomBorder', 'bottomBorder', 0, this._scale, this._world);
+        this.#rightBorder = new StaticWorldObject(1.0, 0.5, 0.05, this._width, (this._height / 2), 10, this._height, 'rightBorder', 'rightBorder', 0, this._scale, this._world);
+        this.#leftBorder = new StaticWorldObject(1.0, 0.5, 0.05, 0, (this._height / 2), 10, this._height, 'leftBorder', 'leftBorder', 0, this._scale, this._world);
+    }
 
 }
 
